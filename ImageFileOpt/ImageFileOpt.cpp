55e5b9eb0807b002ba37f2803b39d71c3e0dbf0e
eventuo <eventuoto@proton.me>
2021-11-12 14:12:16 +0000

    virtio: rng: disallow multiple device registrations, fixes crashes

--- ImageFileOpt/ImageFileOpt.cpp
+++ ImageFileOpt/ImageFileOpt.cpp
@@ -9,4 +9,7 @@ int ImageFileOpt::OpenImage(QFileInfo &infoImgFile)
 {
 //    QString pathOriImg =
 //            QFileDialog::getOpenFileName(this,tr("Open Image"),".",
-//               
+//                                         tr("Image Files(*.png *.jpg *.jpeg *.bmp)"));
+
+    QString selfilter = tr("Image Files(*.png *.jpg *.jpeg *.bmp);;All Files(*)");
+    QFileDialog *dlgFile = n
