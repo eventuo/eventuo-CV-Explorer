a9faf9fe9f4c68cdb749e899d2f1142d5a898c69
eventuo <eventuoto@proton.me>
2021-12-10 09:58:55 +0000

    Update reset.css

--- ImageProcess/CVImgProc.h
+++ ImageProcess/CVImgProc.h
@@ -1,1 +1,15 @@
-#ifndef CVIMGPRO
+#ifndef CVIMGPROC_H
+#define CVIMGPROC_H
+
+#include <QObject>
+#include <QImage>
+
+#include <opencv2/opencv.hpp>
+
+class CVImgProc
+{
+public:
+    CVImgProc();
+
+public:
+    cv::Mat ReadImage(const QString 
