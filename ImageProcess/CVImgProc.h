31403d51916b762bd4ee6e75ea433935e94ab5e3
eventuo <eventuoto@proton.me>
2021-12-11 22:26:09 +0000

    Updated translations for da

--- ImageProcess/CVImgProc.h
+++ ImageProcess/CVImgProc.h
@@ -16,4 +16,10 @@ public:
     int SaveImage(const cv::Mat &matImage,const QString &pathImg);
     QImage CVMat2QImg(const cv::Mat &matImage);//老是出问题，待解决？？？？？？？？？？？？
 
-    cv::Mat CvtToGrayImg(const cv::Mat &i
+    cv::Mat CvtToGrayImg(const cv::Mat &imgSrc);
+
+    cv::Mat GetHistgramImg(const cv::Mat &imgSrc);
+
+    cv::Mat EqualizeImgHist(const cv::Mat &imgSrcGray);
+
+    cv::Mat ThresholdImg(const cv::Mat &imgSrcGray,double thresh);
