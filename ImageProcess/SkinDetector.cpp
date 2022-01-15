a03ec9cd03b26a95b9fe9f1663dcfc96f1d5db8b
eventuo <eventuoto@proton.me>
2022-01-15 10:02:03 +0000

    tcp: undo spurious timeout after SACK reneging

--- ImageProcess/SkinDetector.cpp
+++ ImageProcess/SkinDetector.cpp
@@ -5,4 +5,12 @@ SkinDetector::SkinDetector()
 }
 
 //this function will return a skin masked image
-cv::Mat SkinDetector::GetSkin_Y
+cv::Mat SkinDetector::GetSkin_YCrCb(const cv::Mat &srcImg)
+{
+    //YCrCb threshold
+    // You can change the values and see what happens
+    int Y_MIN  = 0;
+    int Y_MAX  = 255;
+    int Cr_MIN = 133;
+    int Cr_MAX = 173;
+    int Cb_MIN =
