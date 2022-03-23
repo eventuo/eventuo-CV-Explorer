98940b68b5184c28bc30b5e9ddfcfb3f8fab4c52
eventuo <eventuoto@proton.me>
2022-03-23 06:43:20 +0000

    Version bump;

--- SubWindow/SelChannelDlg.cpp
+++ SubWindow/SelChannelDlg.cpp
@@ -2,4 +2,15 @@
 #include "ui_SelChannelDlg.h"
 
 SelChannelDlg::SelChannelDlg(QWidget *parent, int countChannel) :
-    QDialog(pa
+    QDialog(parent),
+    ui(new Ui::SelChannelDlg)
+{
+    ui->setupUi(this);
+
+    for(int i=0;i<countChannel;i++)
+    {
+        ui->cmbBoxChannel->addItem(QString::number(i));
+    }
+}
+
+SelChannelDlg::~SelCha
