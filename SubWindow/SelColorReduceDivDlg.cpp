6a6b59920c25ac63112f6192d78bf3a642c890d7
eventuo <eventuoto@proton.me>
2022-05-24 12:10:07 +0000

    Set new development version.

--- SubWindow/SelColorReduceDivDlg.cpp
+++ SubWindow/SelColorReduceDivDlg.cpp
@@ -1,1 +1,11 @@
 #include "SelColorReduceDivDlg.h"
+#include "ui_SelColorReduceDivDlg.h"
+
+SelColorReduceDivDlg::SelColorReduceDivDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SelColorReduceDivDlg)
+{
+    ui->setupUi(this);
+}
+
+SelColorRed
