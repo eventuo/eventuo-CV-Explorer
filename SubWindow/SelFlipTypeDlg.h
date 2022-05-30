0c0fc3e53f55aa5b1e16f541cc45c8ef36497725
eventuo <eventuoto@proton.me>
2022-05-30 20:30:05 +0000

    thumb

--- SubWindow/SelFlipTypeDlg.h
+++ SubWindow/SelFlipTypeDlg.h
@@ -13,3 +13,17 @@ class SelFlipTypeDlg : public QDialog
     Q_OBJECT
 
 public:
+    explicit SelFlipTypeDlg(QWidget *parent = 0);
+    ~SelFlipTypeDlg();
+
+public:
+    int typeFlip;
+
+private slots:
+    void on_buttonBox_accepted();
+
+private:
+    Ui::SelFlipTypeDlg *ui;
+};
+
+#endif // SELFLIPTYPEDLG_H
