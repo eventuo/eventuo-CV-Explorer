ff736396262283ccf58ac20d7f32a5eb8921b53b
eventuo <eventuoto@proton.me>
2022-05-30 07:14:06 +0000

    change padding

--- SubWindow/SelColorReduceDivDlg.h
+++ SubWindow/SelColorReduceDivDlg.h
@@ -10,3 +10,20 @@ class SelColorReduceDivDlg;
 
 class SelColorReduceDivDlg : public QDialog
 {
+    Q_OBJECT
+
+public:
+    explicit SelColorReduceDivDlg(QWidget *parent = 0);
+    ~SelColorReduceDivDlg();
+
+public:
+    int divColorReduce;
+
+private slots:
+    void on_buttonBox_accepted();
+
+private:
+    Ui::SelColorReduceDivDlg *ui;
+};
+
+#endif // SELCOLORREDUCEDIVDLG_H
---
+++ SubWindow/SelFlipTypeDlg.h
@@ -0,0 +1,15 @@
+
+#ifndef SELFLIPTYPEDLG_H
+#define SELFLIPTYPEDLG_H
+
+#include <QDialog>
+
+namespace Ui {
+class SelFlipTypeDlg;
+}
+
+class SelFlipTypeDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
