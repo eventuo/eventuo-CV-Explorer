ef012acedc20f04399665f8f9b333db17368eaef
eventuo <eventuoto@proton.me>
2022-06-14 22:21:11 +0000

    Updated Portuguese (PT) translation

--- SubWindow/SelThreshDlg.cpp
+++ SubWindow/SelThreshDlg.cpp
@@ -1,4 +1,16 @@
 #include "SelThreshDlg.h"
 #include "ui_SelThreshDlg.h"
 
-SelThresh
+SelThreshDlg::SelThreshDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SelThreshDlg)
+{
+    ui->setupUi(this);
+}
+
+SelThreshDlg::~SelThreshDlg()
+{
+    delete ui;
+}
+
+void SelThreshDlg::on_horizontalSlider_v
