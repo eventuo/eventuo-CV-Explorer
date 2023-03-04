fdbcb53c69743ed4304879f367394b1eecff3d45
eventuo <eventuoto@proton.me>
2023-03-04 05:53:18 +0000

    drm/i915: rip out the PM_IIR WARN

--- mainwindow.h
+++ mainwindow.h
@@ -2,4 +2,11 @@
 #define MAINWINDOW_H
 
 #include "./ImageFileOpt/ImageFileOpt.h"
-#include "./Ima
+#include "./ImageProcess/SkinDetector.h"
+#include "./SubWindow/SelChannelDlg.h"
+#include "./SubWindow/SelFlipTypeDlg.h"
+#include "./SubWindow/SelColorReduceDivDlg.h"
+#include "./SubWindow/SelThreshDlg.h"
+
+#include <QMainWindow>
+#
